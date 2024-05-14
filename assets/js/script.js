9a793dc3af586a953ebb2cf0c914999bfce4f847
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-05-13 22:21:59 -0700

    img src and div and absolute value and open brackets

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,52 +12,54 @@ const searchHistoryDiv = document.querySelector("#search-history")
 
 function getCurrentWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${API_Key}&units=imperial`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        Cityh2.textContent = data.name
-        Temph4.textContent = `Temp: ${data.main.temp}`;
-        Windh4.textContent = `Wind: ${data.wind.speed}`;
-        Humidityh4.textContent = `Humidity: ${data.main.humidity}`;
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            Cityh2.textContent = data.name
+            Temph4.textContent = `Temp: ${data.main.temp}`;
+            Windh4.textContent = `Wind: ${data.wind.speed}`;
+            Humidityh4.textContent = `Humidity: ${data.main.humidity}`;
+        })
 }
 
 function getForecastWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&appid=${API_Key}&units=imperial`)
-    .then(res => res.json())
-    .then(data => {
-        console.log("Date for 5 day forecast")
-        console.log(data)
+        .then(res => res.json())
+        .then(data => {
+            console.log("Date for 5 day forecast")
+            console.log(data)
 
-        const selectedData = [
-            data.list[0],
-            data.list[8],
-            data.list[16],
-            data.list[24],
-            data.list[32],
-        ]
+            const selectedData = [
+                data.list[0],
+                data.list[8],
+                data.list[16],
+                data.list[24],
+                data.list[32],
+            ]
 
-        console.log(selectedData)
+            console.log(selectedData)
 
-        for(i = 0; i < selectedData.length; i++) {
-            const dateH3 = document.querySelector(`#Date-${i}`);
-            const iconImg = document.querySelector(`#Icon-${i}`);
+            for (i = 0; i < selectedData.length; i++) {
+                const dateH3 = document.querySelector(`#Date-${i}`);
+                const iconDiv = document.querySelector(`#Icon-${i}`);
 
-            const TempH5 = document.querySelector(`#Temp-${i}`);
-            const HumidityH5 = document.querySelector(`#Humidity-${i}`);
-            const WindH5 = document.querySelector(`#Wind-${i}`);
+                const TempH5 = document.querySelector(`#Temp-${i}`);
+                const HumidityH5 = document.querySelector(`#Humidity-${i}`);
+                const WindH5 = document.querySelector(`#Wind-${i}`);
 
-            dateH3.textContent = dayjs(selectedData[i].dt * 1000).format("MM/DD/YYYY");
+                dateH3.textContent = dayjs(selectedData[i].dt * 1000).format("MM/DD/YYYY");
+                const iconImg = document.createElement("img");
+                iconDiv.appendChild(iconImg);
+                iconImg.alt = "forecast"
+                iconImg.src = `https://openweathermap.org/img/w/${selectedData[i].weather[0].icon}.png`;
+                //icon.textContent = selectedData[i].dt_txt
+                TempH5.textContent = `Temp: ${selectedData[i].main.temp}`;
+                HumidityH5.textContent = `Humidity: ${selectedData[i].main.humidity}`;
+                WindH5.textContent = `Wind Speed: ${selectedData[i].wind.speed}`
+            }
 
-            iconImg.src =`https://openweathermap.org/img/w/${selectedData[i].weather[0].icon}.png`;
-            //icon.textContent = selectedData[i].dt_txt
-            TempH5.textContent = `Temp: ${selectedData[i].main.temp}`;
-            HumidityH5.textContent = `Humidity: ${selectedData[i].main.humidity}`;
-            WindH5.textContent = `Wind Speed: ${selectedData[i].wind.speed}`
-        }
 
-
-    })
+        })
 }
 
 
@@ -68,20 +70,19 @@ searchButton.addEventListener("click", function () {
     getCurrentWeather();
     getForecastWeather()
 
-    const citiesArr = JSON.parse(localStorage.getItem('Cityh2'));
+    const citiesArr = JSON.parse(localStorage.getItem('Cityh2'))|| []; 
     citiesArr.push(cityInput.value);
 
-    localStorage.setItem('Cityh2', JSON.stringify(citiesArr));  
+    localStorage.setItem('Cityh2', JSON.stringify(citiesArr));
 })
-
-
-const savedCities = JSON.parse(localStorage.getItem('Cityh2'));
+// invoke a function
+const savedCities = JSON.parse(localStorage.getItem('Cityh2'))|| [];
 //                      0            1             2
 
-for(i = 0; i < savedCities.length; i++) {
+for (i = 0; i < savedCities.length; i++) {
     let citiesBtn = document.createElement("button");
     citiesBtn.textContent = savedCities[i];
-    citiesBtn.addEventListener("click", function(event) {
+    citiesBtn.addEventListener("click", function (event) {
 
         console.log(event.target.textContent)
 
@@ -96,7 +97,7 @@ for(i = 0; i < savedCities.length; i++) {
     searchHistoryDiv.append(citiesBtn);
 }
 
-// <button>New York</button>
+
 
 
 
